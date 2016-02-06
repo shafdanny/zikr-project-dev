9294897a29ad5681223d627ac6c5d3533b148876
Shafiq Daniel <shafdanny@gmail.com>
2016-02-06 22:12:44 +0100

    Remove unused elements

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,15 +20,10 @@ class Header extends Component {
     return (
       <div className={s.root}>
         <div className={s.container}>
-          <Navigation className={s.nav} />
           <a className={s.brand} href="/" onClick={Link.handleClick}>
             <img src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>Zikr</span>
           </a>
-          <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
-          </div>
         </div>
       </div>
     );
