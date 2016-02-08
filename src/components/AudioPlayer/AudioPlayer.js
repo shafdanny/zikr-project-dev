9dbd628821408f28390305ea5c49c161c3db8c07
Shafiq Daniel <shafdanny@gmail.com>
2016-02-09 00:42:23 +0100

    Repass test

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -53,7 +53,7 @@ class AudioPlayer extends Component {
           this.state.play();
         } else {
           console.log('end of audio');
-          pause();
+          this.state.pause();
         }
       },
     };
