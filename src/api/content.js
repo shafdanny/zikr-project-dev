0aae378db6e55fd56f9663b19d6a084907520710
Shafiq Daniel <shafdanny@gmail.com>
2016-02-08 22:03:00 +0100

    Fix some test errors

--- src/api/content.js
+++ src/api/content.js
@@ -32,7 +32,6 @@ const fileExists = filename => new Promise(resolve => {
 const router = new Router();
 
 router.get('/', async (req, res, next) => {
-
   try {
     const path = req.query.path;
 
--- src/api/media.js
+++ src/api/media.js
@@ -1,37 +1,33 @@
-import express from 'express';
+import { Router } from 'express';
 import fs from 'fs';
 import { join } from 'path';
 
-
-
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+const router = new Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/list', function(req, res) {
-	console.log(req.path);
-	var media = {
-    	audio: [], video:[]
-	};
+router.get('/list', function (req, res) {
+  console.log(req.path);
+  const media = {
+    audio: [],
+    video: [],
+  };
 
-	fs.readdirSync(join(__dirname, 'public/audio')).forEach(function(file) {
-        console.log(file);
-        media.audio.push({ fileName: file, type:"audio"});
+  fs.readdirSync(join(__dirname, 'public/audio')).forEach(function (file) {
+    console.log(file);
+    media.audio.push({ fileName: file, type: 'audio' });
+  });
 
-    });
+  fs.readdirSync(join(__dirname, 'public/video')).forEach(function (file) {
+    console.log(file);
+    media.video.push({ fileName: file, type: 'video' });
+  });
 
-	fs.readdirSync(join(__dirname, 'public/video')).forEach(function(file) {
-        console.log(file);
-        media.video.push({ fileName: file, type:"video"});
-    });
+  console.log(media);
 
-	console.log(media);
-
-    res.send(media);   
+  res.send(media);
 });
 
 // more routes for our API will happen here
-
-
 export default router;
